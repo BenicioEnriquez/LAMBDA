ace429d2abcaefa16ec316d0f59ea336ae66f099
Benicio Enriquez <54722205+QuantumTitan99@users.noreply.github.com>
2020-01-14 09:45:34 -0800

    Added New Scene

---
+++ Tension/Assets/Scripts/RobotController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    private Rigidbody rig;
+    private GameObject head;
+
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rig.AddRelativeForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rig.AddRelativeForce(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rig.AddRelativeForce(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rig.AddRelativeForce(Vector3.right * speed);
+        }
+    }
+}
