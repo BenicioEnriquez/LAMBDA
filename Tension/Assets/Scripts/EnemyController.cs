24c111bf201c62143f06edf974d1bda5d2ba16be
Benicio Enriquez <54722205+QuantumTitan99@users.noreply.github.com>
2020-01-30 15:51:53 -0800

    Added Enemy

---
+++ Tension/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+    private Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position);
+        rig.AddRelativeForce(Vector3.forward * speed);
+    }
+}
